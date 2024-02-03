d83de67f93673ed11f375fedced8965ce570c9bc
zuohy <zuohukanamatatayang@gmail.com>
2024-02-04 00:52:53 +0800

    add.ts类型

---
+++ 01_TypeScript语法基础/03_HelloTypeScript.ts
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @Author: zuohy
+ * @Date: 2024-02-03 23:27:19
+ * @LastEditors: zuohy
+ * @LastEditTime: 2024-02-04 00:52:19
+ */
+//String: javasctipt中字符串的包装类
+//string: ts给我们定义标识符时提供的字符串类型
+let message:string = "Hello world!";
+
+export {}
